2c0e92af5a9e0ddd52b14e6ca54a4789f55cc02f
Jacob Fraizer <jacob33c@gmail.com>
2019-08-10 19:44:04 -0700

    hover "view project"

--- public/client.js
+++ public/client.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
       keyboard:true
   });
 });
+
+$(".card-img-top").mouseover(function() {
+  console.log("card image has been hovered");
+  $('.carousel-caption').removeClass("hidden");
+  $('.card-img-top').addClass("blurry");
+});
+
+$(".card").mouseleave(function() {
+  console.log("card has been hovered");
+  $('.carousel-caption').addClass("hidden");
+  $('.card-img-top').removeClass("blurry");
+
+});
